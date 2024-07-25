256a443bfdd6ae0f856b60d056d5c4877bb5a497
ernestngugi <ngugi_ernest@yahoo.com>
2024-07-25 14:13:27 +0300

    add application middlewares

---
+++ internal/web/middleware/middleware_test.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/ernestngugi/medvice-backend/internal/testutils"
+	"github.com/gin-gonic/gin"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMiddleware(t *testing.T) {
+
+	Convey("TestMiddleware", t, func() {
+
+
+		testRouter := gin.Default()
+		testRouter.Use(DefaultMiddlewares()...)
+
+		Convey("can set request id", func() {
+			w, err := testutils.DoRequest(testRouter, http.MethodGet, "test-router", nil)
+			So(err, ShouldBeNil)
+
+			So(w.Header().Get("x-request-id"), ShouldNotBeEmpty)
+		})
+
+		Convey("can set application cors", func() {
+			w, err := testutils.DoRequest(testRouter, http.MethodGet, "test-router", nil)
+			So(err, ShouldBeNil)
+
+			So(w.Header().Get("Access-Control-Allow-Origin"), ShouldEqual, "*")
+			So(w.Header().Get("Access-Control-Allow-Credentials"), ShouldEqual, "true")
+			So(w.Header().Get("Access-Control-Allow-Methods"), ShouldEqual, "POST, OPTIONS, GET, PUT, DELETE")
+		})
+	})
+}
